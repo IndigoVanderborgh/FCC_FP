20291c36dae5d19d02925d12bfb70eff54bd5bd1
IndigoVanderborgh <indiorgh@gmail.com>
2024-03-06 18:02:00 -0800

    Update exercise_5.js

--- exercise_5.js
+++ exercise_5.js
@@ -23,9 +23,8 @@
 let fixedValue = 4;
 
 // Only change code below this line
-function incrementer(arg) {
-    let newValue = arg;
-    return newValue = 1 + fixedValue
+function incrementer(value) {
+    return value + 1
   // Only change code above this line
 }
 
